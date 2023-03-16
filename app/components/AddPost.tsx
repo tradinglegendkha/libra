87ede24d01a5aa8a41b5cccd57c762be74040089
Buppi Khan <khanbuppi@gmail.com>
2023-03-15 23:00:52 -0400

    small update, added word limit

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function CreatePost() {
   const [title, setTitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
   return (
     <form className="bg-white my-8 p-8 rounded-md">
       <div className="flex flex-col my-4">
@@ -14,9 +15,15 @@ export default function CreatePost() {
           className="p-4 text-lg rounded-md my-2 bg-gray-200 "
         ></textarea>
       </div>
-      <div>
+      <div className="flex items-center justify-between gap-2">
+        <p
+          className={`font-bold text-sm ${
+            title.length > 300 ? "text-red-700" : "text-gray-700"
+          }`}
+        >{`${title.length}/300`}</p>
         <button
-          className="text-sm bg-teal-600 text-white py-2 px-6"
+          disabled={isDisabled}
+          className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25"
           type="submit"
         >
           Create a post
